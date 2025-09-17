1c15102ad283f0d377893c3a73fd3286669bfab2
touseef zahid <itstouseef38@gmail.com>
2025-09-17 15:35:47 +0500

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -30,5 +30,6 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: "Server Error", error: err.message });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
+
 app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
